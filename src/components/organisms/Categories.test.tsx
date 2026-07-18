afd347ec56ed055756169f965e7aba20511ececd
Kris <johndoe@example.com>
2026-07-18 22:48:29 +0100

    Failing test has been added for the loading spinner to disappear after categories are fetched.

--- src/components/organisms/Categories.test.tsx
+++ src/components/organisms/Categories.test.tsx
@@ -1,11 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 
 import Categories from "./Categories";
 
 describe("Tests for the Categories component", () => {
-  it("As the page is loading, a loading spinner should be displayed while the categories are being fetched.", () => {
+  it("Replace the categories loading spinner with six categories", async () => {
+    // Initially expect the spinner to be present.
     render(<Categories />);
-    expect(screen.getByTestId("spinner-container")).toBeDefined();
+    expect(screen.getByTestId("spinner-container")).toBeInTheDocument();
+
+    // Later we expect the spinner to disappear.
+    const categoriesContainer = await screen.findByTestId(
+      "categories-container",
+    );
+    expect(screen.queryByTestId("spinner-container")).not.toBeInTheDocument();
+    expect(within(categoriesContainer).getAllByTestId("category")).toHaveLength(
+      6,
+    );
   });
 });
