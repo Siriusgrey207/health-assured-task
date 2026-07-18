bec10e05730afcb7c899cda1075f4b56e4f7d51e
Kris <johndoe@example.com>
2026-07-18 21:01:28 +0100

    Render the loading indicator while fetching resources.

--- src/components/organisms/Categories.tsx
+++ src/components/organisms/Categories.tsx
@@ -1,13 +1,13 @@
 export default function Categories() {
   const isPending = true;
 
-  //   if (isPending) {
-  //     return (
-  //       <section>
-  //         <div data-testid="spinner-container"></div>
-  //       </section>
-  //     );
-  //   }
+  if (isPending) {
+    return (
+      <section>
+        <div data-testid="spinner-container">Loading...</div>
+      </section>
+    );
+  }
 
   return <div>Categories</div>;
 }
