984a9c7baadcc9a84e2b9dd138364eb8e52c3856
Kris <johndoe@example.com>
2026-07-21 18:28:35 +0100

    Implemented the sorting algorithm for the sorting of resources by date

--- src/utils/sortResources.ts
+++ src/utils/sortResources.ts
@@ -9,5 +9,17 @@ export function sortResources(
   resources: Resource[],
   sortBy: SortBy,
 ): Resource[] {
-  return [];
+  if (resources.length === 0) return [];
+
+  // Create a copy of the resouces list to prevent direct mutation of the original array.
+  const resourcesCopy = Array.from(resources).sort((a, b) => {
+    const firstDate = new Date(b.date_uploaded);
+    const secondDate = new Date(a.date_uploaded);
+
+    // Use the Date to sort the resources.
+    if (sortBy === "newest") return firstDate.getDate() - secondDate.getDate();
+    else return secondDate.getDate() - firstDate.getDate();
+  });
+
+  return resourcesCopy;
 }
